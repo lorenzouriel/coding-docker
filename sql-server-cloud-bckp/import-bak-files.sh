aa501959908d41fcc28180642635c107badd3ecd
Lorenzo Uriel <lorenzouriel394@gmail.com>
2025-06-19 03:35:30 -0300

    fix: reorganize steps from cloud backup

--- sql-server-cloud-bckp/import-bak-files.sh
+++ sql-server-cloud-bckp/import-bak-files.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# Definir variáveis de ambiente
 BLOB_STORAGE_URL=$BLOB_STORAGE_URL
 DEST_DIR=$DEST_DIR
 AZCOPY_CMD=$AZCOPY_CMD
 
-echo "Baixando os backups do Blob Storage para o diretório de destino..."
+echo "Downloading backups from Blob Storage"
 
-# Comando para copiar arquivos do Blob Storage para o diretório de destino
 $AZCOPY_CMD cp "$BLOB_STORAGE_URL" $DEST_DIR --recursive=true
 
-# Verificar se o comando foi bem-sucedido
 if [ $? -eq 0 ]; then
-  echo "Backups baixados com sucesso!"
+  echo "Backups downloaded succesfully!"
 else
-  echo "Erro ao baixar os backups."
+  echo "Error when downloading."
   exit 1
 fi
--- sql-server-cloud-bckp/restore-databases.sh
+++ sql-server-cloud-bckp/restore-databases.sh
@@ -1,37 +1,23 @@
 #!/bin/bash
 
-# export $(cat .env | xargs)
-
-# Função para verificar se o SQL Server está disponível
-echo "Verificando se o SQL Server está disponível..."
+echo "Checking if SQL is up..."
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$MSSQL_SA_PASSWORD" -Q "SELECT 1" > /dev/null 2>&1
 
-# Combina os dois restores em um único script com um GO
-echo "Restaurando os bancos de dados..."
+echo "Restoring database..."
 
 /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$MSSQL_SA_PASSWORD" -Q "
--- Restauração do banco de dados principal
 RESTORE DATABASE [waves] 
-FROM DISK = N'/var/opt/mssql/backup/fullbckps/waves.bak' 
+FROM DISK = N'/var/opt/mssql/backup/fullbckps/example.bak' 
 WITH 
-    MOVE N'waves' TO N'/var/opt/mssql/data/waves.mdf',
+    MOVE N'waves' TO N'/var/opt/mssql/data/example.mdf',
     MOVE N'COLD' TO N'/var/opt/mssql/data/COLD.ndf',
     MOVE N'WARM' TO N'/var/opt/mssql/data/WARM.ndf',
     MOVE N'HOT' TO N'/var/opt/mssql/data/HOT.ndf',
-    MOVE N'waves_log' TO N'/var/opt/mssql/data/waves_log.ldf',  
+    MOVE N'example_log' TO N'/var/opt/mssql/data/example_log.ldf',  
     NOUNLOAD,  
     STATS = 5;
 
-GO
-
-RESTORE DATABASE [waves.aux] 
-FROM DISK = N'/var/opt/mssql/backup/fullbckps/waves.aux.bak' 
-WITH 
-    MOVE N'waves.aux' TO N'/var/opt/mssql/data/waves.aux_Primary.mdf',  
-    MOVE N'waves.aux_log' TO N'/var/opt/mssql/data/waves.aux_Primary.ldf',  
-    NOUNLOAD,  
-    STATS = 5;
 GO
 "
 
-echo "Restauração dos bancos de dados concluída."
+echo "Database restored succesfully!"
